fb99d4988f99a0ccb0db54fc5672af58a32dc3f5
Pavel Antonov <pehas@list.ru>
2015-05-03 08:37:22 +0000

    docker compose + redis

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var Redis = require('ioredis');
 
 var PORT = 8080
 
 var app = express();
 app.get('/:name', function(req, res){
-  res.send('My super awesome name is - ' + req.param('name') + '!!!!!!!!!!!!!!!!!\n');
+  var port = process.env.DB_PORT;
+  var ip = process.env['DB_PORT_'+port+'_TCP_ADDR'];
+  var redis = new Redis(port, ip);
+  redis.incr('cnt').then(function(cnt){
+    res.send('My awesome name is - ' + req.param('name') + ' ' + cnt + '\n');
+  });
 });
 
 app.listen(PORT);
